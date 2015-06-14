e5f6f3baf6a16c70322e5a03a6e27b452c106a43
unknown <subrato@subrato-W7-1.corp.adobe.com>
2015-06-14 20:56:23 +0530

    Added Few Changes

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -12,8 +12,16 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MAIN_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+	os.path.join(MAIN_DIR, 'static'),
+	)
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -54,7 +62,7 @@ ROOT_URLCONF = 'coffeedapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(MAIN_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,12 +115,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-TEMPLATE_DIRS = (
-	os.path.join(MAIN_DIR, 'templates'),
-	)
 
-STATIC_DIRS = (
-	os.path.join(MAIN_DIR, 'static'),
-	)
+	
 
